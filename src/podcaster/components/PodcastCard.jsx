275957c39c6bd3ea6f2a1c5b1563b68189fde918
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-03-21 13:43:06 +0100

    desestructuracion datos del podcast

--- src/podcaster/components/PodcastCard.jsx
+++ src/podcaster/components/PodcastCard.jsx
@@ -4,22 +4,22 @@ import PropTypes from 'prop-types';
 export const PodcastCard = ({ podcasts }) => {
 	return (
 		<>
-			{podcasts.map(podcast => (
+			{podcasts.map(({ id, imagen, title, artist }) => (
 				<Link
-					to={`/podcast/${podcast.id}`}
-					key={podcast.id}
+					to={`/podcast/${id}`}
+					key={id}
 					className='w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow mb-10 transition ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-slate-50 duration-10'>
 					<div className='flex flex-col items-center pb-6'>
 						<img
 							className='w-24 h-24 mb-3 -mt-10 rounded-full shadow-lg'
-							src={podcast.imagen}
-							alt={`${podcast.title} image`}
+							src={imagen}
+							alt={`${title} image`}
 						/>
 						<h5 className='mb-1 text-md font-semibold text-gray-900 uppercase text-center'>
-							{podcast.title}
+							{title}
 						</h5>
 						<span className='text-sm text-gray-500 text-center'>
-							{podcast.artist}
+							{artist}
 						</span>
 					</div>
 				</Link>
