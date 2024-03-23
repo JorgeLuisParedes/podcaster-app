1d37f91fc89cc169b56442ce99abd071c1eb776f
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-03-23 17:22:14 +0100

    localStorage

--- src/store/slices/podcaster/thunks.js
+++ src/store/slices/podcaster/thunks.js
@@ -1,30 +1,47 @@
 import { podcastApi } from "../../../api";
-import { setPodcasts, startLoadingPodcast } from "./podcasterSlice"
+import { setPodcasts, startLoadingPodcast } from "./podcasterSlice";
 
 export const getPodcast = () => {
 	return async (dispatch, getState) => {
+		const lastFetched = localStorage.getItem("lastFetched");
+		const now = new Date().getTime();
+
+		if (lastFetched && now - lastFetched < 24 * 60 * 60 * 1000) {
+			const storedPodcasts = localStorage.getItem("podcasts");
+			if (storedPodcasts) {
+				dispatch(setPodcasts({ podcasts: JSON.parse(storedPodcasts) }));
+			}
+			return;
+		}
+
 		dispatch(startLoadingPodcast());
-		const { data } = await podcastApi.get();
 
-		const dataApi = data.feed.entry;
-		const transformData = (dataApi) => {
-			return dataApi.map(item => {
-				const largeImage = item['im:image'].sort((a, b) => {
-					return parseInt(b.attributes.height) - parseInt(a.attributes.height);
-				})[0].label;
+		try {
+			const { data } = await podcastApi.get();
+			const dataApi = data.feed.entry;
+			const transformData = (dataApi) => {
+				return dataApi.map(item => {
+					const largeImage = item['im:image'].sort((a, b) => {
+						return parseInt(b.attributes.height) - parseInt(a.attributes.height);
+					})[0].label;
 
-				return {
-					id: item.id.attributes['im:id'],
-					title: item['im:name'].label,
-					artist: item['im:artist'].label,
-					description: item.summary.label,
-					imagen: largeImage
-				};
+					return {
+						id: item.id.attributes['im:id'],
+						title: item['im:name'].label,
+						artist: item['im:artist'].label,
+						description: item.summary.label,
+						imagen: largeImage
+					};
+				});
+			};
+			const podcastList = transformData(dataApi);
 
-			});
-		};
-		const podcastList = transformData(dataApi);
+			localStorage.setItem("podcasts", JSON.stringify(podcastList));
+			localStorage.setItem("lastFetched", now.toString());
 
-		dispatch(setPodcasts({ podcasts: podcastList }));
+			dispatch(setPodcasts({ podcasts: podcastList }));
+		} catch (error) {
+			console.error("Error fetching podcasts: ", error);
+		}
 	}
-}
+};
