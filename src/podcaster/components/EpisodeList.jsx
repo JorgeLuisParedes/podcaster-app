ee0b008f02715a8d1539a2e396fa87c5b6691a85
Jorge Luis Paredes <paredesjorgeluis@gmail.com>
2024-03-29 18:08:32 +0100

    margin bottom al final de la tabla

--- src/podcaster/components/EpisodeList.jsx
+++ src/podcaster/components/EpisodeList.jsx
@@ -10,7 +10,7 @@ export const EpisodeList = ({ podcastId, episodes }) => {
 						Episodes: {episodes.length}
 					</h2>
 				</div>
-				<div className='w-11/12 mx-auto py-7 px-3 border border-gray-200 rounded-md shadow'>
+				<div className='w-11/12 mx-auto py-7 px-3 border border-gray-200 rounded-md shadow mb-7'>
 					<div className='relative overflow-x-auto'>
 						<table className='w-full text-sm text-left text-gray-500'>
 							<thead className='text-xs text-gray-700 uppercase bg-gray-50'>
